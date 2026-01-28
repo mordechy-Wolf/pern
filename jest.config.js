c21fb51e9d2fb2eb6964c7aa5fc39473892e4227
mordechy-Wolf <770nmy@gmail.com>
2026-01-28 12:21:31 +0200

    תיאור השינוי שעשית

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',          // זה החלק החשוב – מפעיל ts-jest
+  preset: 'ts-jest',         
   testEnvironment: 'node',
   testTimeout: 30000,
   clearMocks: true,
   moduleFileExtensions: ['ts', 'js'],
-  testMatch: ['**/tests/**/*.test.[jt]s'], // תופס גם .ts וגם .js
+  testMatch: ['**/tests/**/*.test.[jt]s'], 
 };
